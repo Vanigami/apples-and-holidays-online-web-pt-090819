d4603708bb05152d71a0f4aa6ed3a8d8cef78adc
Vanna Winland <vgwinland@gmail.com>
2019-09-22 23:49:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supplies|
-      if supplies.include?("BBQ")
+      holiday if supplies.include?("BBQ")
     end
   end.flatten.compact
 end
