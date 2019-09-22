95369eeeb6e3338459c983f50b6c23f1d5c60c08
Vanna Winland <vgwinland@gmail.com>
2019-09-22 23:38:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |holiday, supplies|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      
+      puts ""
 
 end
 
